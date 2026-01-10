51fbbf985eec18d06963d92e23f966c5e4f921a1
apipsmu2000 <apipsmu@gmail.com>
2026-01-11 03:11:50 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'erph-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Network First - Utamakan Internet, kalau takde baru guna Cache)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
